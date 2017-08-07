650e93f8498738d9bacdc892c3c9bad4b9401b69
Tayler Blake <tayler.a.blake@gmail.com>
2017-08-07 13:40:12 -0400

    clean up from the pre-JSM fury with lack of version control

--- code/lib/fit_cholesky_PS.R
+++ code/lib/fit_cholesky_PS.R
@@ -1,31 +1,28 @@
-fit_cholesky_PS <- function(Y,
-                            y_aug,
+fit_cholesky_PS <- function(yVec,
+                            N,
                             U,
                             D,
                             P_l,
                             lambda_l,
                             P_m,
                             lambda_m,
-                            lambda_ridge,
-                            P_l_shape, lambda_l_shape){
-  N.subjects <- nrow(Y)
-  if (!is.null(y_aug)) {
-    yVec <- as.vector(c(y_aug,
-                        t(Y[,-1])))
-  }
-  
-  if (is.null(y_aug)) {
-    yVec <- as.vector(t(Y[,-1]))
-  }
+                            lambda_ridge){
+  # N.subjects <- nrow(Y)
+  # if (!is.null(y_aug)) {
+  #   yVec <- as.vector(c(y_aug,
+  #                       t(Y[,-1])))
+  # }
+  # 
+  # if (is.null(y_aug)) {
+  #   yVec <- as.vector(t(Y[,-1]))
+  # }
   Pen <- rbind(lambda_l*P_l,
                lambda_m*P_m)
+  
   if (lambda_ridge > 0) {
-    Pen <- rbind(Pen,lambda_ridge*diag(ncol(P_l)))
+    Pen <- rbind(Pen,lambda_ridge*diag(ncol(U)))
   }
 
-  if (lambda_l_shape > 0) {
-    Pen <- rbind(Pen,lambda_l_shape*P_l_shape)
-  }
   n.col <- ncol(U)
   nix <- rep(0,nrow(Pen))
   
@@ -45,15 +42,9 @@ fit_cholesky_PS <- function(Y,
     
     mu <- eta
     h.prime <- 1
-    if (is.null(y_aug)) {
-      w <- rep(1, length(yVec))
-    }
+ 
+    w <- rep(1/diag(D)[-1]^2,N)
         
-    if (!is.null(y_aug)) {
-      w <- c(rep(0,length(y_aug)),
-             rep(1/diag(D)[-1]^2,N.subjects))      
-    }
-
     u <- (yVec - mu)/h.prime + eta
     
     startTS <- Sys.time()
