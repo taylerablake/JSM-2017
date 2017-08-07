847908753d808e01967ed3c3dd41ea5af07e9f03
Tayler Blake <tayler.a.blake@gmail.com>
2017-08-07 07:38:54 -0400

    make a picture to show the weird domain of phi

--- code/src/knot-grid-img-script.R
+++ code/src/knot-grid-img-script.R
@@ -24,8 +24,8 @@ myGrid <- myGrid %>% transform(.,l_s=l/max(myGrid$l),
 # B-spline parameters
 bdeg = 3  # B-spline degree
 
-nsegl = 12  # number of inner knots to build B-spline bases for x1 and x2
-nsegm = 12
+nsegl = 15  # number of inner knots to build B-spline bases for x1 and x2
+nsegm = 15
 
 Bl <- bspline(myGrid$l_s, min(myGrid$l_s),
               max(myGrid$l_s),
@@ -48,8 +48,6 @@ B. <- kronecker(Bm,
                                                            Bl)
 
 
-
-
       
 expand.grid(t=seq(min(lKnots),max(lKnots),length.out=length(lKnots)),
             s=seq(min(lKnots),max(lKnots),length.out=length(lKnots))) %>%
@@ -68,22 +66,53 @@ expand.grid(t=seq(min(lKnots),max(lKnots),length.out=length(lKnots)),
       theme(panel.grid.minor=element_blank()) 
       
 
-which(colSums(B. != 0) == 0) 
-knot_grid <- expand.grid(m=mKnots,l=lKnots)[,2:1]
+
+
+
+      
+      knot_grid <- data.frame(expand.grid(l=lKnots,m=mKnots),
+                              expand.grid(l_index=1:length(lKnots),
+                                          m_index=1:length(mKnots)))
+      
+
+      keep <- (knot_grid$m >= 0.5) & (knot_grid$m <= sort(unique(knot_grid$l),
+                                                          decreasing = TRUE)[2]-(0.5*knot_grid$l)) |
+            (knot_grid$m <= 0.5) & (knot_grid$m >= sort(unique(knot_grid$l))[2]+(0.5*knot_grid$l))
+      keep[knot_grid$l_index==1] <- FALSE
+      keep[knot_grid$m_index==max(knot_grid$m_index)-1] <- FALSE 
+      keep[knot_grid$l_index==max(knot_grid$l_index)-1] <- FALSE
+      keep[knot_grid$l==max(knot_grid$l)] <- FALSE
+
+      
+      for(this_l in unique(knot_grid$l_index[knot_grid$keep==TRUE & (knot_grid$l_index%%2==0)])){
+            knot_grid$keep[knot_grid$l_index==this_l & knot_grid$m_index > (max(knot_grid$m_index)-this_l)] <- FALSE
+      }      
+      
+      knot_grid <- transform(knot_grid,keep=factor(keep))
+      
+      knot_grid[knot_grid$keep==TRUE & (knot_grid$l_index%%2==0),] 
+
 ggplot(knot_grid,aes(l,m)) +
-      geom_point(size=0.5) +
+      geom_point(data=subset(knot_grid,keep==TRUE),
+                 aes(l,m),
+                 size=0.8,
+                 alpha=0.85) +
+      geom_point(data=subset(knot_grid,keep==FALSE),
+                 aes(l,m),
+                 size=0.5,
+                 alpha=0.2) +
       theme_minimal() +
       xlab("l") +
       ylab("m") +
-      scale_x_continuous(breaks = seq(0,1.2,by=0.2)) +
-      scale_y_continuous(breaks = seq(0,1.2,by=0.2)) +
+      scale_x_continuous(breaks = seq(0,1,by=0.2)) +
+      scale_y_continuous(breaks = seq(0,1,by=0.2)) +
       geom_polygon(data=data.frame(l=c(0,1,0),m=c(0,0.5,1)),
                    aes(l,m),
                    colour="light grey",
                    fill="light grey",
                    alpha=0.4) +
       theme(panel.grid.minor=element_blank())
-
+ggsave(file.path(getwd(),"TeX","img","knot-removal-on-triangle-domain.png"))
 
 
 ggplot(knot_grid,aes(l,m)) +
